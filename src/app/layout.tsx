127df2837b42bbe73a83be8123171b07290c55c4
HamzaMuhd <hamzamuhdidris@gmail.com>
2023-10-02 11:36:43 +0100

    changed project background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Providers } from "@/components/providers/Providers";
 
 export const metadata: Metadata = {
   title: 'CodeWithJameel',
-  description: 'Strong community of javascript developers',
+  description: 'A community of passionate coders, eager to learn the latest tech trends, and looking to connect with like-minded individuals. CodeWithJameel is a gateway to a vibrant and supportive community of developers from all corners of Nigeria.',
 }
 
 export default function RootLayout({
