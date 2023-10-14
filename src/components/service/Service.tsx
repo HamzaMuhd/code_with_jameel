aad3bcb08b0e14ac8e613ba92ec753b3487952a4
Muhammad Ayuba Bello <talk2muhammadbello@gmail.com>
2023-10-14 18:05:12 +0100

    editing homepage sections

--- src/components/service/Service.tsx
+++ src/components/service/Service.tsx
@@ -25,78 +25,80 @@ const Service = () => {
             px='16px'
         >
             <VStack>
-            <Heading className={roboto.className}>Our Mission</Heading>
-            <Text 
-                mt={5}
-                mb={10}
-                fontSize={18}    
-                className={josefin_Sans.className}
-                textAlign={{ base:'center', md:'start' }}       
-            >Elevating Nigerian Tech Talent and Fostering Innovation through our 3 E&apos;s</Text>
-            <HStack 
-            flexDirection={{base:"column", md:"row"}}
-            gap={50}>
-                <Box 
-                    w={{base:"100%", md:"370px"}}
-                    maxW={{base:"350px", md:"370px"}}
-                    h={{ base:"100%", md:"330px" }}
-                    bg='#D9D9D9'
-                    borderRadius="15px"
-                    display='flex'
-                    justifyContent='center'
-                    alignItems='center'
-                    flexDir='column'
-                    p='30px'
-                    gap={3}
-                    mx='14px'
+                <Heading className={roboto.className}>Our Mission</Heading>
+                <Text 
+                    mt={5}
+                    mb={10}
+                    fontSize={18}    
+                    className={josefin_Sans.className}
+                    textAlign={{ base:'center', md:'start' }}       
+                >Elevating Nigerian Tech Talent and Fostering Innovation through our 3 E&apos;s</Text>
+                <HStack 
+                    flexDirection={{base:"column", md:"row"}}
+                    gap={50}
+                    w='100%'
                 >
+                    <Box 
+                        w={{base:"100%", md:"370px"}}
+                        maxW={{base:"350px", md:"370px"}}
+                        h={{ base:"100%", md:"330px" }}
+                        bg='#D9D9D9'
+                        borderRadius="15px"
+                        display='flex'
+                        justifyContent='center'
+                        alignItems='center'
+                        flexDir='column'
+                        p='30px'
+                        gap={3}
+                        mx='14px'
+                    >
+                        <div>
+                            <RiGraduationCapFill size={48} /> {/* Adjust the size as needed */}
+                        </div>
+                        <Heading textAlign="justify" className={roboto.className}>Education</Heading>
+                        <Text textAlign={{ base:'center', md:'start' }} className={josefin_Sans.className}>We&apos;re dedicated to offering top-notch tech education to Nigerian youth, equipping them with expertise to excel in the digital era through structured curriculum and hands-on experience.</Text>
+                    </Box>
+                    <Box 
+                        w={{base:"100%", md:"370px"}}
+                        maxW={{base:"350px", md:"370px"}}
+                        h={{ base:"100%", md:"330px" }}
+                        bg='#D9D9D9'
+                        borderRadius="15px"
+                        display='flex'
+                        justifyContent='center'
+                        alignItems='center'
+                        flexDir='column'
+                        p='30px'
+                        gap={3}
+                        mx='14px'
+                    >
                     <div>
-                        <RiGraduationCapFill size={48} /> {/* Adjust the size as needed */}
-                    </div>
-                    <Heading textAlign="justify" className={roboto.className}>Education</Heading>
-                    <Text textAlign={{ base:'center', md:'start' }} className={josefin_Sans.className}>We&apos;re dedicated to offering top-notch tech education to Nigerian youth, equipping them with expertise to excel in the digital era through structured curriculum and hands-on experience.</Text>
-                </Box>
-                <Box 
-                    w={{base:"100%", md:"370px"}}
-                    maxW={{base:"350px", md:"370px"}}
-                    h={{ base:"100%", md:"330px" }}
-                    bg='#D9D9D9'
-                    borderRadius="15px"
-                    display='flex'
-                    justifyContent='center'
-                    alignItems='center'
-                    flexDir='column'
-                    p='30px'
-                    gap={3}
-                    mx='14px'
-                >
-                   <div>
-                        <RiTimeFill size={48} />{/* Adjust the size as needed */}
-                    </div>
-                    <Heading textAlign="justify" className={roboto.className}>Experience</Heading>
-                    <Text textAlign={{ base:'center', md:'start' }} className={josefin_Sans.className}>We provide a supportive environment for students to apply their knowledge in real-world scenarios through projects, hackathons, and mentorship, paving their path to success</Text>
-                </Box>
-                <Box 
-                    w={{base:"100%", md:"370px"}}
-                    maxW={{base:"350px", md:"370px"}}
-                    h={{ base:"100%", md:"330px" }}
-                    bg='#D9D9D9'
-                    borderRadius="15px"
-                    display='flex'
-                    justifyContent='center'
-                    alignItems='center'
-                    flexDir='column'
-                    p='30px'
-                    gap={3}
-                    mx='14px'
-                >
-                    <div>
-                        <RiBriefcaseFill size={48} /> {/* Adjust the size as needed */}
-                    </div>
-                    <Heading textAlign="justify" className={roboto.className}>Employement</Heading>
-                    <Text textAlign={{ base:'center', md:'start' }} className={josefin_Sans.className} >We prepare students for the job market and connect them with tech industry opportunities. Partnering with businesses, we create employment pathways and internships. </Text>
-                </Box>
-            </HStack>
+                            <RiTimeFill size={48} />{/* Adjust the size as needed */}
+                        </div>
+                        <Heading textAlign="justify" className={roboto.className}>Experience</Heading>
+                        <Text textAlign={{ base:'center', md:'start' }} className={josefin_Sans.className}>We provide a supportive environment for students to apply their knowledge in real-world scenarios through projects, hackathons, and mentorship, paving their path to success</Text>
+                    </Box>
+                    <Box 
+                        w={{base:"100%", md:"370px"}}
+                        maxW={{base:"350px", md:"370px"}}
+                        h={{ base:"100%", md:"330px" }}
+                        bg='#D9D9D9'
+                        borderRadius="15px"
+                        display='flex'
+                        justifyContent='center'
+                        alignItems='center'
+                        flexDir='column'
+                        p='30px'
+                        gap={3}
+                        mx='14px'
+                    >
+                        <div>
+                            <RiBriefcaseFill size={48} /> {/* Adjust the size as needed */}
+                        </div>
+                        <Heading textAlign="justify" className={roboto.className}>Employement</Heading>
+                        <Text textAlign={{ base:'center', md:'start' }} className={josefin_Sans.className} >We prepare students for the job market and connect them with tech industry opportunities. Partnering with businesses, we create employment pathways and internships. </Text>
+                    </Box>
+                </HStack>
             </VStack>
         </Box>
     );
